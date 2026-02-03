3e40924857ec57128daeae1c8bbc846694aff0b7
rpitts2 <rpitts2@vcu.edu>
2026-02-03 18:07:27 -0500

    inital commit -set up r project, gitignore, and set up R script header

---
+++ plot_mtcars.R
@@ -0,0 +1,14 @@
+
+# HGEN 612 Project 1
+# Rachel Pitts
+# Feb. 4 2026
+
+###################################
+
+# load required packages
+
+library(ggplot2)
+
+# load mtcars dataset
+
+data("mtcars")
