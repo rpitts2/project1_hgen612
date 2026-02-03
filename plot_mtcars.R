89f5c93cf48c30d7e1b264c4fd40be0f9941474a
rpitts2 <rpitts2@vcu.edu>
2026-02-03 18:29:35 -0500

    Changed theme to theme_classic

--- plot_mtcars.R
+++ plot_mtcars.R
@@ -20,4 +20,6 @@ data("mtcars")
 
 ggplot(mtcars,
        aes(x = mpg)) +
- geom_histogram(binwidth = 3, fill = "orange", color = "black")
+ geom_histogram(binwidth = 3, fill = "orange", color = "black") +
+ theme_classic()  
+
