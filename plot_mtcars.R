3a3bc54661e3e986082682e98cb2a060b7e1ea8a
rpitts2 <rpitts2@vcu.edu>
2026-02-03 18:21:11 -0500

    Added inital histogram plot for mpg variable

--- plot_mtcars.R
+++ plot_mtcars.R
@@ -12,3 +12,12 @@ library(ggplot2)
 # load mtcars dataset
 
 data("mtcars")
+
+?mtcars  # 32 observations, 11 numeric variables
+
+
+# initial plot
+
+ggplot(mtcars,
+       aes(x = mpg)) +
+ geom_histogram()
